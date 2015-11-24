aee5c9323e3bc8d4ce618500be285529cae3b1d7
U-DESKTOP-FG7TI8O\rotomoMBPwin <Roberttmoon@gmail.com>
2015-11-23 23:51:53 -0600

    adding dishes

---
+++ DishWasher/Dish.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DishWasher
+{
+    public abstract class Dish
+    {
+        int weight;
+        bool machineWash;
+        bool clean;
+    }
+}
