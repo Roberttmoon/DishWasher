8eb226e511c9b8ee68232792a7befc8e3dae43ed
roberttmoon@gmail.com <roberttmoon@gmail.com>
2015-11-24 09:42:29 -0600

    updated the class name to better reflect the use

---
+++ DishWasher/Spoon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DishWasher
+{
+    class Spoon : Silverware, ICanHoldLiquid
+    {
+        public void canHoldLiquid()
+        {
+            Console.WriteLine("Im Silverware that can hold liquid");
+        }
+    }
+}
